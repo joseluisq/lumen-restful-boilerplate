222831bea8131d579cb849235eba9498b176f41e
José Quintana <joseluisquintana20@gmail.com>
2016-08-31 16:44:47 -0500

    Update Handler.php

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -58,7 +58,6 @@ class Handler extends ExceptionHandler {
     if ($e instanceof HttpResponseException) {
       $success = FALSE;
       $status = Response::HTTP_INTERNAL_SERVER_ERROR;
-      $response = $e->getResponse();
     } elseif ($e instanceof MethodNotAllowedHttpException) {
       $success = FALSE;
       $status = Response::HTTP_METHOD_NOT_ALLOWED;
@@ -71,15 +70,6 @@ class Handler extends ExceptionHandler {
       $success = FALSE;
       $status = Response::HTTP_FORBIDDEN;
       $e = new AuthorizationException('You do not have the necessary permissions for the resource.', $status);
-    } elseif ($e instanceof \Dotenv\Exception\ValidationException && $e->getResponse()) {
-      $success = FALSE;
-      $status = Response::HTTP_BAD_REQUEST;
-      $e = new \Dotenv\Exception\ValidationException('The server cannot or will not process the request due to an apparent client error.', $status, $e);
-      $response = $e->getResponse();
-    } elseif ($e) {
-      $success = FALSE;
-      $status = Response::HTTP_INTERNAL_SERVER_ERROR;
-      $e = new HttpException($status, 'It has occurred an unexpected server error.');
     }
 
     $res = response()->json([
